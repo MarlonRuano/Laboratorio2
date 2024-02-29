f83843e72b0f94a9ef1a485c1163f81e1d5541b0
MarlonRuano <159932872+MarlonRuano@users.noreply.github.com>
2024-02-29 16:08:34 -0600

    Add files via upload

---
+++ metodostatic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodo_static_marlon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int resultado = SumarNumeros();
+                Console.WriteLine("La suma de los números es: " + resultado);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("¡Gracias por usar la aplicación!");
+            }
+        }
+        static int SumarNumeros()
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            string entrada1 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el segundo número:");
+            string entrada2 = Console.ReadLine();
+            int num1, num2;
+            try
+            {
+                num1 = Convert.ToInt32(entrada1);
+                num2 = Convert.ToInt32(entrada2);
+            }
+            catch (FormatException)
+            {
+                throw new Exception("Debe ingresar números válidos.");
+            }
+            catch (OverflowException)
+            {
+                throw new Exception("Los números ingresados están fuera del rango válido.");
+            }
+
+            return num1 + num2;
+        }
+    }
+}
