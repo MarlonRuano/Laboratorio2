67e84796f912472761a5349decd1db1902bc5bc1
MarlonRuano <159932872+MarlonRuano@users.noreply.github.com>
2024-02-29 15:28:22 -0600

    Add files via upload

--- ingresos.cs
+++ ingresos.cs
@@ -4,35 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace inresos_marlon
+using System;
+
+namespace La_Calculadora
 {
-    internal class Program
+    class Calculadora
+    {
+        
+        public int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Restar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Error: No se puede dividir por cero.");
+                return 0;
+            }
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Por favor, ingresa tus ingresos de los últimos 3 meses:");
+            Calculadora calculadora = new Calculadora();
 
-            double[] ingresos = new double[3];
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Write($"Ingresos del mes {i + 1}: ");
-                ingresos[i] = Convert.ToDouble(Console.ReadLine());
-            }
+            Console.Write("Ingrese el primer número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Por favor, escribe tu nombre completo: ");
-            string nombreCompleto = Console.ReadLine();
+            Console.Write("Ingrese el segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            double sumaIngresos = 0;
-            foreach (double ingreso in ingresos)
-            {
-                sumaIngresos += ingreso;
-            }
-            double promedioIngresos = sumaIngresos / 3;
-
-            Console.WriteLine($"Hola {nombreCompleto}, en total ganaste una suma de {sumaIngresos} y promediaste un promedio de {promedioIngresos}");
-
-            Console.ReadLine(); 
-
+            Console.WriteLine("Suma: " + calculadora.Sumar(num1, num2));
+            Console.WriteLine("Resta: " + calculadora.Restar(num1, num2));
+            Console.WriteLine("División: " + calculadora.Dividir(num1, num2));
+            Console.WriteLine("Multiplicación: " + calculadora.Multiplicar(num1, num2));
+            Console.ReadLine();
         }
     }
 }
